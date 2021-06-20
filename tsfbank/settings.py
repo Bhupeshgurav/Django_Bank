b11d85bb04e3d06aadc0233a57c1f08df93f44ed
Bhupesh Gurav <bhupeshgurav124@gmail.com>
2021-06-20 19:29:05 +0530

    initial commit

--- tsfbank/settings.py
+++ tsfbank/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = 'django-insecure-k+&d+sfj_95y2ghcw*ulh($&=)fgm+j^4k5a6bpl(ua&r25jw)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://djangotsfbank.herokuapp.com']
 
 
 # Application definition
